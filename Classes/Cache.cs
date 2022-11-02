72332644875cab96a67493f5225f5d1b5869ee57
schelip <fcominscheffel@gmail.com>
2022-11-01 22:52:18 -0300

    small fixes and docs

--- Classes/Cache.cs
+++ Classes/Cache.cs
@@ -3,8 +3,14 @@ using static MultiprocessorSimulator.Logging;
 
 namespace MultiprocessorSimulator
 {
+    /// <summary>
+    /// Classe abstrata de cache, sem algoritmo de substituição definido
+    /// </summary>
     public abstract class Cache
     {
+        /// <summary>
+        /// Vetor com as instâncias das unidades de cache
+        /// </summary>
         public static Cache[] CacheUnits = new Cache[CACHE_UNITS];
 
         public enum MESI
@@ -15,6 +21,10 @@ namespace MultiprocessorSimulator
             INVALID = 'I'
         }
 
+        /// <summary>
+        /// Eventos disparados durante a leitura e escrita, para gerenciamento do MESI
+        /// </summary>
+        /// <param name="tag">Identificador do bloco na memória sendo lido/alterado</param>
         public delegate void CacheEvent(int tag);
         public event CacheEvent? ReadEvent;
         public event CacheEvent? WriteEvent;
@@ -35,6 +45,7 @@ namespace MultiprocessorSimulator
             for (int i = 0; i < CACHE_SETS; i++)
                 _sets[i] = new CacheSet();
 
+            // Inscrição de todas as caches aos eventos de todas as outras unidades
             foreach (Cache c in Cache.CacheUnits.Where(c => c != null && c != this))
             {
                 c.ReadEvent += HandleReadEvent;
@@ -44,6 +55,10 @@ namespace MultiprocessorSimulator
             }
         }
 
+        /// <summary>
+        /// Instancia as unidades de uma subclasse específica de cache
+        /// </summary>
+        /// <typeparam name="T">Classe concreta implementando o algoritmo</typeparam>
         public static void InitializeUnits<T>() where T : Cache, new()
         {
             for (int i = 0; i < CACHE_UNITS; i++)
@@ -51,8 +66,17 @@ namespace MultiprocessorSimulator
             PrintInfo(UNITS_INITIALIZED, CACHE_UNITS);
         }
 
+        /// <summary>
+        /// Algortimo de substituição
+        /// </summary>
+        /// <param name="target">Referência à linha que deve ser substituída</param>
+        /// <returns>Índice da linha que deve ser substituída</returns>
         protected abstract int GetReplacementSet(out CacheSet target);
 
+        /// <summary>
+        /// Encontra linha não utilizada (inválida) ou utiliza algoritmo de substituição
+        /// </summary>
+        /// <returns>Índice da linha que será utilizada</returns>
         protected virtual int GetTargetSet()
         {
             CacheSet target;
@@ -70,6 +94,10 @@ namespace MultiprocessorSimulator
             return i;
         }
 
+        /// <summary>
+        /// Salva dados de uma linha na memória
+        /// </summary>
+        /// <param name="set">Linha a ser salva</param>
         protected void WriteBack(ref CacheSet set)
         {
             PrintInfo(SAVING_DATA, set.Tag);
@@ -78,19 +106,29 @@ namespace MultiprocessorSimulator
             PrintMemoryLines(5, set.Tag);
         }
 
+        /// <summary>
+        /// Altera dados de uma linha com os dados de uma nova posição
+        /// </summary>
+        /// <param name="idx">índice da linha a ser alterada</param>
+        /// <param name="ra">ReadAddress da palavra a ser acessada</param>
+        /// <param name="set">Referência da linha a ser alterada</param>
+        /// <returns>Se foi feito um acesso a memória</returns>
         protected bool UpdateSet(int idx, ReadAddress ra, out CacheSet set)
         {
             set = this._sets[idx];
 
+            // Se os dados que serão substituídos foram modificados, salvá-los
             if (set.Status == MESI.MODIFIED)
                 WriteBack(ref set);
 
             set.Tag = ra.Tag;
             Block? block = CheckExclusive(this, set);
+            // Se alguma outra unidade de cache tem o mesmo bloco, copiá-lo
             if (block.HasValue) {
                 set.Block = block.Value;
                 return false;
             } else {
+            // Se não, acessar a memória
                 PrintInfo(READING_MEMORY, ra.Tag);
                 PrintMemoryLines(5, ra.Tag);
                 set.Block = Memory.Read(ra.Tag);
@@ -98,13 +136,20 @@ namespace MultiprocessorSimulator
             }
         }
 
+        /// <summary>
+        /// Realiza as operações necessárias para trazer os dados de um bloco para a cache
+        /// </summary>
+        /// <param name="ra">ReadAddress da palavra a ser acessada</param>
+        /// <param name="target">Referência a linha com a palavra acessada</param>
+        /// <param name="hit">Se ocorreu um HIT ou MISS</param>
+        /// <param name="exclusive">Se foi utilizada uma cópia da linha de outra cache</param>
         protected void Access(ReadAddress ra, out CacheSet target, out bool hit, out bool exclusive)
         {
-            int idx = -1;
             exclusive = true;
             for (int i = 0; i < _sets.Length; i++)
             {
                 CacheSet set = _sets[i];
+                // Se o bloco está presente em uma linha válida, HIT
                 if (set.Tag == ra.Tag && set.Status != MESI.INVALID)
                 {
                     PrintInfo(HIT);
@@ -113,16 +158,18 @@ namespace MultiprocessorSimulator
                     return;
                 }
             }
+            // Se não, MISS
             PrintInfo(MISS);
             hit = false;
 
+            // Emite evento de leitura
             CacheEvent? readEvent = ReadEvent;
             if (readEvent != null) {
                 PrintInfo(READ_REQUEST, GetCacheUnitIndex(), ra.Tag);
                 readEvent(ra.Tag);
             }
 
-            idx = idx < 0 ? GetTargetSet() : idx;
+            int idx = GetTargetSet();
             exclusive = UpdateSet(idx, ra, out target);
             
             PrintInfo(SET_UPDATED, idx, ra.Tag);
@@ -130,11 +177,18 @@ namespace MultiprocessorSimulator
             return;
         }
 
+        /// <summary>
+        /// Lê certa palavra para a cache
+        /// </summary>
+        /// <param name="ra">ReadAddress da palavra a ser lida</param>
+        /// <returns>Valor da palavra lida</returns>
         public int Read(ReadAddress ra)
         {
             PrintInfo(READ);
             Access(ra, out CacheSet set, out bool hit, out bool exclusive);
 
+            // Se não estava presente, alterar status para EXCLUSIVE se não o bloco não é acessado
+            // por nenhuma outra unidade; caso contrário, para SHARED
             if (!hit)
                 ChangeSetStatus(set, exclusive ? MESI.EXCLUSIVE : MESI.SHARED);
                 
@@ -143,6 +197,12 @@ namespace MultiprocessorSimulator
             return set.Block.Read(ra.Offset);
         }
 
+        /// <summary>
+        /// Lê e altera certa palavra na cache
+        /// </summary>
+        /// <param name="ra">ReadAddress da palavra a ser alterada</param>
+        /// <param name="newValue">Novo valor para a palavra</param>
+        /// <returns>Valor da palavra após alteração</returns>
         public int Write(ReadAddress ra, int newValue)
         {
             PrintInfo(WRITE);
@@ -150,6 +210,8 @@ namespace MultiprocessorSimulator
             set.Block.Write(newValue, ra.Offset);
             PrintInfo(REPLACING_WORD, ra.Offset, newValue);
 
+            // Se não estava presente ou alguma outra unidade tem uma cópia do mesmo dado,
+            // emite evento de escrita
             if (!hit || set.Status == MESI.SHARED)
             {
                 CacheEvent? writeEvent = WriteEvent;
@@ -160,6 +222,7 @@ namespace MultiprocessorSimulator
                 }
             }
             
+            // Marca linha como modificada, status MODIFIED
             ChangeSetStatus(set, MESI.MODIFIED);
 
             PrintCaches();
@@ -167,8 +230,17 @@ namespace MultiprocessorSimulator
             return set.Block.Read(ra.Offset);
         }
 
+        /// <summary>
+        /// Helper para encontrar índice da cache na lista de unidades
+        /// </summary>
+        /// <returns>índice da cache</returns>
         public int GetCacheUnitIndex() => Cache.CacheUnits.ToList().IndexOf(this);
 
+        /// <summary>
+        /// Helper para alterar status de uma linha da cache
+        /// </summary>
+        /// <param name="set">Linha a ter status alterado</param>
+        /// <param name="status">Novo status para a linha</param>
         public void ChangeSetStatus(CacheSet set, MESI status)
         {
             if (set.Status != status)
@@ -178,6 +250,12 @@ namespace MultiprocessorSimulator
             }
         }
 
+        /// <summary>
+        /// Verifica se a linha existe em outra unidade de cache
+        /// </summary>
+        /// <param name="cache">Cache que deseja acessar uma cópia</param>
+        /// <param name="set">Linha que será procurada</param>
+        /// <returns>Dados da linha, caso exista, ou null</returns>
         public static Block? CheckExclusive(Cache cache, CacheSet set)
         {
             int ci = 0, si = 0;
@@ -198,6 +276,11 @@ namespace MultiprocessorSimulator
             return null;
         }
 
+        /// <summary>
+        /// Verifica se ocorreu um snoop read, ou seja, se outra cache leu os dados de um bloco
+        /// presente nessa unidade
+        /// </summary>
+        /// <param name="tag">Identificador do bloco lido</param>
         public void HandleReadEvent(int tag)
         {
             for (int i = 0; i < this._sets.Length; i++)
@@ -205,14 +288,21 @@ namespace MultiprocessorSimulator
                 CacheSet set = this._sets[i];
                 if (set.Tag == tag)
                 {
+                    // Se havia uma cópia modificada, salvar modificações (dirty writeback)
                     if (set.Status == MESI.MODIFIED)
                         WriteBack(ref set);
+                    // Se os dados estão válidos, marcar como SHARED
                     if (set.Status != MESI.INVALID)
                         ChangeSetStatus(set, MESI.SHARED);
                 }
             }
         }
 
+        /// <summary>
+        /// Verifica se ocooreu um snoop write, ou seja, se outra cache alterou os dados de um bloco
+        /// presente nessa unidade
+        /// </summary>
+        /// <param name="tag">Identificador do bloco lido na memória</param>
         public void HandleWriteEvent(int tag)
         {
             for (int i = 0; i < this._sets.Length; i++)
@@ -220,8 +310,10 @@ namespace MultiprocessorSimulator
                 CacheSet set = this._sets[i];
                 if (set.Tag == tag)
                 {
+                    // Se havia uma cópia modificada, salvar modificações (dirty writeback)
                     if (set.Status == MESI.MODIFIED)
                         WriteBack(ref set);
+                    // Marcar dados dessa unidade como INVALID, visto que estarão desatualizados
                     ChangeSetStatus(set, MESI.INVALID);
                 }
             }
@@ -229,6 +321,9 @@ namespace MultiprocessorSimulator
         }
     }
 
+    /// <summary>
+    /// Implementação concreta de cache com algoritmo de substituição FIFO utilizando buffer circular
+    /// </summary>
     public class FIFOCache : Cache
     {
         private int[] _buffer = new int[CACHE_SETS];
--- Classes/Config.cs
+++ Classes/Config.cs
@@ -2,7 +2,7 @@ namespace MultiprocessorSimulator
 {
     public static class Config
     {
-        public const int TAGS = 50;
+        public const int TAGS = 256;
         public const int BLOCK_WORDS = 4;
         public const int CACHE_SETS = 4;
         public const int MAX_MEMORY_RANDOM = 999;
